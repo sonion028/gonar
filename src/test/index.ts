78cea4b0873750d7bf7e9f3851dc287bf7cd5ac5
sonion028 <sonion028@gmail.com>
2026-01-01 00:18:12 +0800

    test: 添加字符串转换工具的测试用例

---
+++ src/test/index.ts
@@ -0,0 +1,31 @@
+import {
+  convertCamel2Pascal,
+  convertPascal2Camel,
+  convertCamel2Snake,
+  convertSnake2Camel,
+} from '../utils';
+
+const testObj = {
+  camelCaseKey: 'value',
+  PascalCaseKey: 'value',
+  snake_case_key: 'value',
+  nestedObj: {
+    camelCaseKey2: 'value',
+    PascalCaseKe5y2: 'value',
+    snake_ca6se_key2: 'value',
+    nextArray: [
+      {
+        camelCaseKey: 'value',
+        PascalCaseKey: 'value',
+        snake_case_key: 'value',
+      },
+    ],
+  },
+};
+
+const testObjSnake = convertCamel2Snake([testObj, testObj.nestedObj]);
+const testObjCamel = convertSnake2Camel(testObjSnake);
+const testObjPascal = convertCamel2Pascal(testObj);
+const testObjCamel2 = convertPascal2Camel(testObjPascal);
+
+console.log(testObjSnake, testObjCamel, testObjPascal, testObjCamel2);
