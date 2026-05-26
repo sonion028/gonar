0405e7cc72f84c2fe785787ee9e9305bd3ed2167
Mr.r <sonion028@gmail.com>
2026-05-26 21:22:54 +0800

    build: 替换rolldown为rollup作为构建工具
    将项目构建工具从rolldown切换到rollup，新增相关rollup插件依赖，移除rolldown相关依赖，新增rollup配置文件并调整tsconfig配置项

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'rolldown';
+import { defineConfig } from 'rollup';
 import path from 'path';
-import { dts } from 'rolldown-plugin-dts';
-import cssRolldown from 'rolldown-plugin-css';
+import { fileURLToPath } from 'url';
+import alias from '@rollup/plugin-alias';
+import resolve from '@rollup/plugin-node-resolve';
+import { swc } from 'rollup-plugin-swc3';
+import terser from '@rollup/plugin-terser';
+import { dts } from 'rollup-plugin-dts';
+import { cssRollup } from 'rolldown-plugin-css';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // 入口 重复提取公共
 const INPUTS = {
@@ -13,26 +20,28 @@ const INPUTS = {
 
 export default defineConfig([
   {
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
-      },
-    },
     input: INPUTS,
     output: {
       dir: 'dist',
       format: 'esm',
-      entryFileNames: '[name].[format].js', // 不用后缀
+      entryFileNames: '[name].esm.js',
       chunkFileNames: 'js/[name].[hash].js', // 除入口外的 chunk 文件放js文件夹
-      minify: true, // 压缩 js 文件
       // 设置生成的代码目标版本
       generatedCode: {
-        preset: 'es5', // 可选值: 'es5' | 'es2015'
+        preset: 'es2015',
         symbols: true,
       },
     },
     external: ['react', 'react-dom', 'react/jsx-runtime'],
-    plugins: [cssRolldown({ minify: true })],
+    plugins: [
+      alias({
+        entries: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+      }),
+      resolve({ extensions: ['.ts', '.tsx', '.js', '.jsx', '.mjs'] }),
+      swc(),
+      cssRollup(),
+      terser(),
+    ],
   },
   // dts 类型声明生成
   {
@@ -45,19 +54,6 @@ export default defineConfig([
         // preserveModules: true, // 不打包保留目录结构，结合之生成.d.ts配置，是类型声明代目录结构
       },
     ],
-    plugins: [
-      ...dts({
-        tsconfig: 'tsconfig.json',
-        emitDtsOnly: true,
-        tsgo: true,
-        // 以下oxc需要，需要显式返回，不推荐
-        // oxc: true,
-        // resolver: 'oxc',
-        // compilerOptions: {
-        //   declaration: true, // 生成类型声明
-        //   isolatedDeclarations: true, // 生成隔离的类型声明，要求显式的返回类型
-        // },
-      }),
-    ],
+    plugins: [dts({ tsconfig: 'tsconfig.json' })],
   },
 ]);
