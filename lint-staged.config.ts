7b8ec3402e9132c02b4398f25d2dc42ab8558bb6
Mr.r <sonion028@gmail.com>
2026-06-10 12:43:59 +0800

    build(lint-staged): update lint check config
    1. remove redundant src directory tsc check rule
    2. simplify eslint rule scope to only packages/apps
    3. combine and optimize lint command sequence

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -3,9 +3,7 @@ import { getStagedPackagesTscCmd } from './scripts/staged-packages/index.ts';
 export default {
   // 仅检查受影响的子包
   '{packages,apps}/**/*.{ts,tsx,vue}': getStagedPackagesTscCmd,
-  // tsc 检查只能整个包，不能只检查某个文件
-  'src/**/*.{ts,tsx,vue}': () => ['tsc --noEmit'],
-  '{src,packages,apps}/**/*.{js,jsx,ts,tsx,vue}': ['eslint --fix'],
-  '{src,packages,apps}/**/*.{js,jsx,ts,tsx,vue,css,scss,less,md,mdx,html,json,yml,yaml}':
+  '{packages,apps}/**/*.{js,jsx,ts,tsx,vue}': 'eslint --fix',
+  '{packages,apps}/**/*.{js,jsx,ts,tsx,vue,css,scss,less,md,mdx,html,json,yml,yaml}':
     ['oxfmt', 'cspell lint'],
 };
