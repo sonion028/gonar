0fc4290f8b09b4b69cf357de919322f1f40789b4
sonion028 <sonion028@gmail.com>
2026-07-11 00:04:22 +0800

    build(react): 更新打包入口文件路径
    将components、hooks、utils的打包入口从单文件改为对应目录下的index.ts

--- packages/react/tsdown.config.ts
+++ packages/react/tsdown.config.ts
@@ -3,9 +3,9 @@ import createTsdownConfig from '../../tsdown.config.base.ts';
 export default createTsdownConfig({
   entry: {
     index: 'src/index.ts',
-    components: 'src/components.ts',
-    hooks: 'src/hooks.ts',
-    utils: 'src/utils.ts',
+    components: 'src/components/index.ts',
+    hooks: 'src/hooks/index.ts',
+    utils: 'src/utils/index.ts',
   },
   deps: {
     neverBundle: [/^@tonar\/react-kit(?:\/.+)?$/, '@tonar/utils'],
